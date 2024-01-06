33da6444614cbe6ad80b5203e30ebc434bbf2fc6
nbush <nbush257@gmail.com>
2024-01-06 15:39:57 -0800

    add the calibrate sketch

--- calibrate/calibrate.ino
+++ calibrate/calibrate.ino
@@ -3,19 +3,14 @@
 
 Cobalt cobalt;
 Tbox tbox;
-LASER_AMP = 0.75; //This should map to about 50-10mW at fiber tip.
 
 void setup() {  
-  
+  tbox.begin();
   cobalt.begin();
 }
 
 void loop() {
-  while(Serial.available()){Serial.read();}
-  Serial.println("Press enter to begin test");
-  while(!Serial.available()){}
-  
-  cobalt.phasic_stim_insp(15, LASER_AMP, 10000, 10000)
-
+  tbox.user_wait();
+  cobalt.calibrate_high_res();
 
 }
